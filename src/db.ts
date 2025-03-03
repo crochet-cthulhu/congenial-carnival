d1e52bcf0d9d281b23ad373c168b0afb2fb8d8e7
crochet-cthulu <sahesmh@outlook.com>
2025-03-03 15:37:09 +0000

    Added database handler for caching playlists

--- src/db.ts
+++ src/db.ts
@@ -4,6 +4,8 @@
  */
 import { MongoClient } from 'mongodb';
 import Event from './models/event';
+import Playlist from './models/playlist';
+import * as Types from './types';
 
 const dbConnectionString = process.env.MONGODB_CONNSTRING;
 const dbClient = new MongoClient(dbConnectionString);
@@ -21,6 +23,17 @@ export async function addEventToDb(event: string) {
 
     const eventToInsert : Event = new Event(event, new Date().getTime());
     events.insertOne(eventToInsert);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function addPlaylistToDb(playlistData: Types.playlistData, tracks: Types.trackParent[]) {
+  try {
+    const database = dbClient.db(dbName);
+    const playlists = database.collection('playlists');
+    const playlist = new Playlist(playlistData, tracks);
+    playlists.findOneAndUpdate({ 'playlist.id': playlistData.id }, { $set: playlist }, { upsert: true });
   } catch (error) {
     console.error(error);
   }
